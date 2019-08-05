72cd45dd052360533ad5e42734195aba159b79d3
EdLo <b93303015@gmail.com>
2019-08-05 17:44:46 +0800

    Slice big txt file

--- handler.go
+++ handler.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
+	"strings"
 )
 
 func preProcess(filename string) {
@@ -11,17 +13,36 @@ func preProcess(filename string) {
 
 	txt := string(data)
 	// log.Println(string(data))
+	result := cut(txt)
+	// log.Println(len(result))
 
-	if len(txt) >= 5000 {
-		cut()
-	} else {
-		//TODO
+	for _, v := range result {
+		log.Println(v)
+		log.Println("--------------------")
 	}
+
 }
 
-func cut() []string {
-	//TODO
-	return nil
+func cut(txt string) []string {
+
+	result := make([]string, 0)
+
+	s := strings.Fields(txt)
+
+	b := new(strings.Builder)
+
+	for _, v := range s {
+		// log.Println(v)
+		b.WriteString(v + " ")
+		// log.Println(b.Len())
+		if b.Len() > 4900 {
+			result = append(result, b.String())
+			b.Reset()
+		}
+	}
+	result = append(result, b.String())
+
+	return result
 }
 
 func merge(data []byte, filename string) {
